41aa85e1124447f4646a8c1bc7abf92afc345534
KseniaFedoseeva <Ks1887@yandex.ru>
2022-05-04 21:05:24 +0500

    test

--- src/test/java/ru/fedoseeva/selenium/TestSelenium.java
+++ src/test/java/ru/fedoseeva/selenium/TestSelenium.java
@@ -1,16 +1,14 @@
 package ru.fedoseeva.selenium;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TestSelenium {
@@ -18,33 +16,33 @@ public class TestSelenium {
     private WebDriverWait wait;
     private WebDriver driver;
 
-
-    @Test
-    public void tests() {
-
+    @Before
+    public void before(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10, 1000);
 
         // Переход на страницу "Логин"
         driver.navigate().to("http://training.appline.ru/user/login");
         WebElement titleLoginPage = driver.findElement(By.xpath("//h2[@class='title' and contains(text(), 'Логин')]"));
         Assert.assertTrue("Страничка не загрузилась", titleLoginPage.isDisplayed() && titleLoginPage.getText().contains("Логин"));
+    }
+
+    @Test
+    public void tests() {
+
 
         // Авторизация на странице (логин)
-        //    String fieldXPath = "//input[@class='span2']";
         WebElement login = driver.findElement(By.xpath("//input[@name='_username']"));
-        //       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='_username']")));
         login.click();
         login.sendKeys("Секретарь");
         Assert.assertEquals("Секретарь", "Секретарь");
-        // wait.until(ExpectedConditions.textToBe(By.xpath("//input[@name='_username']"), "Секретарь"));
 
         // Авторизация на странице (пароль)
         WebElement password = driver.findElement(By.xpath("//input[@name='_password']"));
-        //       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='_username']")));
         password.click();
         password.sendKeys("testing");
         Assert.assertEquals("testing", "testing");
@@ -75,21 +73,24 @@ public class TestSelenium {
         choseDepartment.click();
         WebElement administrationDepartment = driver.findElement((By.xpath("//option[text() = 'Администрация']")));
         administrationDepartment.click();
+        Assert.assertTrue("Не верно подразделение", administrationDepartment.getText().contains("Администрация"));
 
-       WebElement list = driver.findElement(By.xpath("//a[text() = 'Открыть список']"));
-       try{
-           Thread.sleep(3000);
-       }
-        catch (InterruptedException e){
-           e.printStackTrace();
+        WebElement list = driver.findElement(By.xpath("//a[text() = 'Открыть список']"));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-       list.click();
+        list.click();
 
         WebElement indicateItem = driver.findElement(By.xpath("//span[text() = 'Укажите организацию']"));
         indicateItem.click();
 
-       WebElement choseCompany = driver.findElement((By.xpath("//div[text() = '(Яндекс)Призрачная Организация Охотников']")));
-       choseCompany.click();
+        WebElement choseCompany = driver.findElement((By.xpath("//div[text() = '(Яндекс)Призрачная Организация Охотников']")));
+        choseCompany.click();
+        String comp = "(Яндекс)Призрачная Организация Охотников";
+        WebElement company = driver.findElement(By.name("crm_business_trip[company]"));
+        Assert.assertEquals("Не верна организация", comp, company.getAttribute("value"));
 
 
         WebElement bookindTicket = driver.findElement(By.xpath("//label[text() = 'Заказ билетов']"));
@@ -100,11 +101,13 @@ public class TestSelenium {
         leavingCity.clear();
         String depCity = "Россия, Пермь";
         leavingCity.sendKeys(depCity);
+        Assert.assertEquals("Не верен пункт прибытия", depCity, leavingCity.getAttribute("value"));
 
         WebElement arrivalCity = driver.findElement(By.name("crm_business_trip[arrivalCity]"));
         arrivalCity.click();
         String arrCity = "Россия, Москва";
         arrivalCity.sendKeys(arrCity);
+        Assert.assertEquals("Не верен пункт прибытия", arrCity, arrivalCity.getAttribute("value"));
 
         WebElement dateDeparture = driver.findElement(By.xpath("//input[starts-with(@name, 'date_selector_crm_business_trip_departureDatePlan') " +
                 "and @placeholder = 'Укажите дату']"));
@@ -112,47 +115,31 @@ public class TestSelenium {
         dateDeparture.click();
         String depDate = "20.05.2022";
         dateDeparture.sendKeys(depDate);
-
         dateDeparture.submit();
+        Assert.assertEquals("Не верна дата отправления", depDate, dateDeparture.getAttribute("value"));
 
         WebElement dateArrival = driver.findElement(By.xpath("//input[starts-with(@name, 'date_selector_crm_business_trip_returnDatePlan') " +
                 "and @placeholder = 'Укажите дату']"));
         String arrDate = "26.05.2022";
-              dateArrival.sendKeys(arrDate);
+        dateArrival.sendKeys(arrDate);
         dateArrival.submit();
+        Assert.assertEquals("Не верна дата прибытия", arrDate, dateArrival.getAttribute("value"));
 
+        //Сохранить и закрыть
 
-        // Проверка правильности заполнения полей
-        Assert.assertTrue("Не верно подразделение", administrationDepartment.getText().contains("Администрация"));
-        WebElement company = driver.findElement(By.name("crm_business_trip[company]"));
-        try{
-            Thread.sleep(5000);
-        }
-        catch (InterruptedException e){
-            e.printStackTrace();
-        }
-      //  Assert.assertTrue("Не верна организация", company.equals("(Яндекс)Призрачная Организация Охотников"));
-      //  Assert.assertEquals ("Не верен пункт отправления", depCity,  leavingCity.getAttribute("value"));
-     //   Assert.assertEquals("Не верен пункт прибытия", arrCity, arrCity);
-        scrollToElementJs(dateArrival);
-   //     scrollToElementJs(arrivalCity);
-        Assert.assertEquals("Не верен пункт прибытия", arrCity, arrivalCity.getAttribute("value"));
-      //  Assert.assertTrue("Не верна дата отпраления", dateDeparture.getText().contains(depDate));
-      //  Assert.assertTrue("Не верна дата прибытия", dateArrival.getText().contains(arrDate));
+        WebElement saveAndClose = driver.findElement(By.xpath("//button[@data-action='{\"route\":\"crm_business_trip_index\"}']"));
 
-        //       driver.quit();
+        saveAndClose.submit();
+
+        // Проверка не заполненных полей
+        WebElement listEmploee = driver.findElement(By.xpath("//span[text() = 'Список командируемых сотрудников не может быть пустым']"));
+        listEmploee.isDisplayed();
 
     }
-    private void scrollToElementJs(WebElement element) {
 
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
-        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
-        try{
-            Thread.sleep(5000);
-        }
-        catch (InterruptedException e){
-            e.printStackTrace();
-        }
-
+    @After
+    public void after() {
+        driver.quit();
     }
+
 }
